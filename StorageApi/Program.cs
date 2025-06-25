c1f00d58e5583e927facd88b493a9ff1adb906ee
katja tcareva <katja.tcareva@gmail.com>
2025-06-25 17:58:37 +0200

    Add SwaggerUI

--- StorageApi/Program.cs
+++ StorageApi/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using StorageApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<StorageApiContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("StorageApiContext") ?? throw new InvalidOperationException("Connection string 'StorageApiContext' not found.")));
 
 // Add services to the container.
+builder.Services.AddDbContext<StorageApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StorageApiContext") ?? throw new InvalidOperationException("Connection string 'StorageApiContext' not found.")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -17,7 +18,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Storage API V1");
+    });
 }
+
 
 app.UseHttpsRedirection();
 
